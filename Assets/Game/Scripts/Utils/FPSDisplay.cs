fde7c344422836b1ec21296368cbc84b01614678
Alexander Evstratov <alexdrugev@me.com>
2025-03-27 20:23:51 +0300

    Add FPS.

--- Assets/Game/Scripts/Utils/FPSDisplay.cs
+++ Assets/Game/Scripts/Utils/FPSDisplay.cs
@@ -4,35 +4,33 @@
 //  Copyright (c) 2018 Appic Studio
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class FPSDisplay : MonoBehaviour
 {
-
+	[SerializeField] private Text _fpsText;
 	public Color textClr = Color.white;
 
 	float deltaTime = 0.0f;
-	
-	void Update()
-	{
-		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-	}
-	
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-		
-		GUIStyle style = new GUIStyle();
-		
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = textClr;
-		float msec = deltaTime * 1000.0f;
-		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
-	}
+
+    private float timePassed = 0f;
+    private int frameCount = 0;
+
+    void Update()
+    {
+        timePassed += Time.unscaledDeltaTime;
+        frameCount++;
+
+        if (timePassed >= 0.5f)
+        {
+            float fps = frameCount / timePassed;
+            _fpsText.text = string.Format("{0:0.} FPS", fps);
+
+            timePassed = 0f;
+            frameCount = 0;
+        }
+    }
 }
 
 
